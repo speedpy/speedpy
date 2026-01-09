28b1c7d211fcb27e5dd5cc7bc1f76e0708e7d81f
Kostja P <git@kostja.me>
2026-01-09 23:35:28 +0100

    Add `SALT_KEY` to settings with `.env` support
    - Introduced `SALT_KEY` environment variable with a default value for improved configuration flexibility.

--- project/settings.py
+++ project/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 SECRET_KEY = env("SECRET_KEY", default="change_me")
-
+SALT_KEY = env("SALT_KEY", default="changeme")
 DEBUG = env("DEBUG", default=False)
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["*"])
