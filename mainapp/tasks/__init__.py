9ef42117f450598f73a40597422ef6647c679892
Kostja P <git@kostja.me>
2026-01-10 10:58:54 +0100

    Update `__all__` in `tasks/__init__.py` to include new task exports

--- mainapp/tasks/__init__.py
+++ mainapp/tasks/__init__.py
@@ -1,6 +1,8 @@
 from .teams import *
 
 __all__ = [
-    'send_team_invitation_email',
-    'send_role_change_notification',
+    "send_team_invitation_email",
+    "send_role_change_email",
+    "expire_team_memberships",
+    "expire_team_memberships_invitations",
 ]
