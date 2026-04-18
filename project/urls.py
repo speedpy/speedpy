16e304e9169d4d2f22cecfab761d35cf1b2a0c8d
Kostja P <git@kostja.me>
2026-04-18 17:09:36 +0200

    profile/password change / otp forms and minified picture in the top nav

--- project/urls.py
+++ project/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 from django.conf import settings
+from django.conf.urls.static import static
 from mainapp import views
 import speedpycom.views
 from usermodel.views import ProfileEditView
@@ -17,3 +18,6 @@ urlpatterns = [
     path("__debug__/", include("debug_toolbar.urls")),
     path("", include("mainapp.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- usermodel/models.py
+++ usermodel/models.py
@@ -1,5 +1,8 @@
+import os
 import uuid
+from io import BytesIO
 
+from django.core.files.base import ContentFile
 from django.db import models
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
@@ -7,7 +10,11 @@ from django.core.mail import send_mail
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
 from django.conf import settings
+from PIL import Image
+
 from usermodel.managers import UserManager
+
+PROFILE_THUMBNAIL_SIZE = (96, 96)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -23,6 +30,19 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     first_name = models.CharField(_('First Name'), max_length=50, blank=True)
     last_name = models.CharField(_('Last Name'), max_length=50, blank=True)
+    profile_picture = models.ImageField(
+        _('Profile Picture'),
+        upload_to='profile_pictures/',
+        blank=True,
+        null=True,
+    )
+    profile_picture_thumbnail = models.ImageField(
+        _('Profile Picture Thumbnail'),
+        upload_to='profile_pictures/thumbnails/',
+        blank=True,
+        null=True,
+        editable=False,
+    )
     is_staff = models.BooleanField(
         _('Staff Status'),
         default=False,
@@ -68,3 +88,47 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def email_user(self, subject, message, from_email=None, **kwargs):
         send_mail(subject, message, from_email, recipient_list=[self.email], **kwargs)
+
+    def save(self, *args, **kwargs):
+        regenerate_thumbnail = False
+        if self.profile_picture:
+            if self.pk:
+                previous = type(self).objects.filter(pk=self.pk).first()
+                previous_name = previous.profile_picture.name if previous and previous.profile_picture else ''
+                if previous_name != self.profile_picture.name:
+                    regenerate_thumbnail = True
+            else:
+                regenerate_thumbnail = True
+        elif self.profile_picture_thumbnail:
+            self.profile_picture_thumbnail.delete(save=False)
+
+        if regenerate_thumbnail:
+            self._generate_profile_picture_thumbnail()
+
+        super().save(*args, **kwargs)
+
+    def _generate_profile_picture_thumbnail(self):
+        self.profile_picture.seek(0)
+        image = Image.open(self.profile_picture)
+        has_alpha = image.mode in ('RGBA', 'LA') or (
+            image.mode == 'P' and 'transparency' in image.info
+        )
+        image = image.convert('RGBA' if has_alpha else 'RGB')
+        image.thumbnail(PROFILE_THUMBNAIL_SIZE, Image.Resampling.LANCZOS)
+
+        buffer = BytesIO()
+        if has_alpha:
+            image.save(buffer, format='PNG', optimize=True)
+            extension = 'png'
+        else:
+            image.save(buffer, format='JPEG', quality=85, optimize=True)
+            extension = 'jpg'
+
+        self.profile_picture.seek(0)
+
+        base_name, _ext = os.path.splitext(os.path.basename(self.profile_picture.name))
+        self.profile_picture_thumbnail.save(
+            f"{base_name}_thumb.{extension}",
+            ContentFile(buffer.getvalue()),
+            save=False,
+        )
